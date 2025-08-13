d8f2f799503fdf96db2bf1378f9ae125d89ada66
FlyingSaturn <abc>
2025-08-13 21:27:46 +0530

    Assignment 2 done

---
+++ assign2/remove-occurence.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    long a;
+    const int MAX = 200;
+    char n[MAX];
+    printf("Enter a number: ");
+    scanf("%ld", &a);
+    sprintf(n, "%ld", a);
+    char d;
+    printf("Digit to remove: ");
+    scanf(" %c", &d);
+    int index = -1;
+    for (int i = 0, l = strlen(n); i < l; i++)
+    {
+        if (n[i] == d)
+        {
+            index = i;
+            // C compiler short-circuits it to false
+            // if it sees a "false and ..."
+            if (i < l - 1 && n[i] < n[i + 1])
+                break;
+        }
+    }
+    for (int i = index, l = strlen(n); i < l && i >= 0; i++)
+        n[i] = n[i + 1]; // '\0' also shifts to the left
+    printf("Result: %s\n", n);
+}
