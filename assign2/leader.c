bc7726a36571d711b36894b9e355f6c167401176
FlyingSaturn <56539009+FlyingSaturn@users.noreply.github.com>
2025-08-13 16:22:28 +0530

    Create leader.c

---
+++ assign2/leader.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n; bool greater;
+    printf("n: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the numbers: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    printf("Leader elements: ");
+    int max = a[n - 1];
+    for (int i = n-1; i >= 0; i--)
+    {
+        if (a[i] >= max)
+        {
+            printf("%d", a[i]);
+            max = a[i];
+        }
+    }
+    printf("\n");
+}
